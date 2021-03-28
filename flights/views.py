2bc9c17e67b429e1a2017866da6efa5877453f13
KasperOmari <mohammad.omari852@gmail.com>
2021-03-29 00:39:19 +0300

    Implemented flight application with simple UI

--- flights/views.py
+++ flights/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseBadRequest
+from .models import Flight, Passenger
 
 def index(request):
-    return render(request, "flights/index.html")
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
+
+def flight(request, flight_id):
+    try:
+        flight = Flight.objects.get(pk=flight_id)
+        return render(request, "flights/flight.html", {
+            "flight": flight,
+            "passengers": flight.passengers.all(),
+            "non_passengers": Passenger.objects.exclude(flights=flight).all()
+        })
+    except:
+        return HttpResponseBadRequest("Flight not found")
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return redirect("flights:flight", flight_id=flight_id)
