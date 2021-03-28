0ed6e7aeb006b34801e34af3e64e0099342cdfe5
KasperOmari <mohammad.omari852@gmail.com>
2021-03-29 01:12:30 +0300

    Implemented simple tasks application with simple UI

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,6 @@ app_name = "tasks"
 urlpatterns = [
     path("", views.index, name="index"),
     path("add", views.add, name="add"),
+    path("<int:task_id>", views.task, name="task"),
+    path("<int:task_id>/update", views.update, name="update"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseBadRequest
 from .forms import NewTaskForm
+from .models import Task
 
 def index(request):
-    if "tasks" not in request.session:
-        request.session["tasks"] = []
+    tasks = Task.objects.all()
 
     return render(request, "tasks/index.html", {
-        "tasks": request.session["tasks"]
+        "tasks": tasks
     })
 
 def add(request):
     if request.method == "POST":
         form = NewTaskForm(request.POST)
         if form.is_valid():
-            task = form.cleaned_data["task"]
-            request.session["tasks"] += [task]
+            form.save()
             return redirect("tasks:index")
         else:
             return render(request, "tasks/add.html", {
@@ -24,3 +24,29 @@ def add(request):
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
     })
+
+def task(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+        form = NewTaskForm(instance=task)
+        return render(request, "tasks/task.html", {
+            "task": task,
+            "form": form
+        })
+    except:
+        return HttpResponseBadRequest("Task not found")
+
+def update(request, task_id):
+    if request.method == "POST":
+        task = Task.objects.get(pk=task_id)
+        form = NewTaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("tasks:task", task_id=task_id)
+        else:
+            return render(request, "tasks/task.html", {
+                "task": task,
+                "form": form
+            })
+    else:
+        return HttpResponseBadRequest("Invalid Method")
