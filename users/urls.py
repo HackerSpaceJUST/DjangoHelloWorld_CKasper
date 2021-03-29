ad0cd14e5fe55ea76c361fc5b809822b0c40b955
KasperOmari <mohammad.omari852@gmail.com>
2021-03-30 02:50:40 +0300

    Enabled resgisteration with two different ways
    1. Using 'SignUpView' class. (Recommended)
    2. Writing the resgisteration logic manually.

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from .views import SignUpView
 
 app_name = "users"
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
+    path("signup", SignUpView.as_view(), name="signup"),
+    path("register", views.register_view, name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,17 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.views import generic
 
 def index(request):
     if not request.user.is_authenticated:
         return redirect("users:login")
 
-    return render(request, "users/user.html")
+    return render(request, "users/user.html", {
+        "count": User.objects.count()
+    })
 
 def login_view(request):
     if request.method == "POST":
@@ -20,6 +26,27 @@ def login_view(request):
                 "message": "Invalid credentials."
             })
     return render(request, "users/login.html")
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("users:index")
+        else:
+            return render(request, "users/register.html", {
+                "form": form
+            })
+    
+    form = UserCreationForm()
+    return render(request, "users/register.html", {
+        "form": form
+    })
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = "users/signup.html"
 
 def logout_view(request):
     logout(request)
